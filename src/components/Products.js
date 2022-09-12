6b570caf9168236aeaa4696895c4d3fa0fcf5ea7
Bhavana Nangare <60853406+akshunangare15@users.noreply.github.com>
2022-09-12 01:04:04 -0700

    Add files via upload

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,150 +1,120 @@
 
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router';
 import Skeleton from 'react-loading-skeleton';
 import { NavLink } from 'react-router-dom';
 
-function Product() {
+function Products() {
 
-    const { id } = useParams();
-    const [product, setProduct] = useState([]);
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [filter, setFilter] = useState(data);
 
     useEffect(() => {
-        const getProduct = async () => {
+        let componentMounted = true;
+        const getProdcuts = async () => {
             setLoading(true);
-            const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-            const data = await response.json();
-            setProduct(data);
-            setLoading(false);
+            const response = await fetch('https://fakestoreapi.com/products');
+            if (componentMounted) {
+                const data = await response.json();
+                setData(data);
+                setFilter(data);
+                setLoading(false);
+            }
+            return () => {
+                componentMounted = false;
+            }
         }
-        getProduct();
-    }, [id]);
+        getProdcuts();
+    }, []);
 
     const Loading = () => {
         return (
-            <>
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-12">
-                        <NavLink className="text-decoration-none text-dark" to={`/`}>
-                            <div className="d-flex align-items-center m-3">
-                                <Skeleton height={20} width={50} />
-                            </div>
-                        </NavLink>
-                        <div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="images p-3">
-                                        <div className="text-center p-4">
-                                            <Skeleton height={300} width={250} />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="border p-4">
-                                        <div className="mt-4 mb-3"> <span className="text-uppercase text-muted brand">                                                <Skeleton height={30} width={150} />
-                                        </span>
-                                            <h5 className="text-uppercase">
-                                                <Skeleton height={30} width={200} />
-                                            </h5>
-                                            <div className="price d-flex flex-row align-items-center">
-                                                <span className="act-price">
-                                                    <Skeleton height={20} width={70} />
-                                                    <Skeleton height={30} width={100} />
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <p className="about">
-                                            <Skeleton height={10} width={300} />
-                                            <Skeleton height={10} width={300} />
-                                            <Skeleton height={10} width={300} />
-                                            <Skeleton height={10} width={300} />
-                                        </p>
-                                        <div className="cart mt-4 align-items-center">
-                                            <Skeleton height={40} width={150} />
+         <>
 
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+               <div className="col-md-9 py-md-3">
+                     <div className="row">
+                         <div className="col-1 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div> 
+                         <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
                         </div>
-                    </div>
-                </div>
-            </>
-        )
-    }
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>   
+                   </div>
 
-    const ShowDetails = () => {
+               </div>
+
+
+           </>
+         )
+    
+        }
+    // const filterProduct = (category) => {
+    //     const updateList = data.filter((x) => x.category === category);
+    //     setFilter(updateList);
+    // }
+
+    const ShowProducts = () => {
         return (
             <>
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-12">
-                        <NavLink className="text-decoration-none text-dark" to={`/`}>
-                            <div className="d-flex align-items-center m-3">
-                                <i className="fa fa-long-arrow-left"></i>
-                                <span className="ml-1">&nbsp;Back</span>
-                            </div>
-                        </NavLink>
-                        <div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="images p-3">
-                                        <div className="text-center p-4">
-                                            <img id="main-image" alt="product" src={product.image}  width="250" />
-                                            
-                                             
-                                             
-                                            
-                                        </div>
-                                    </div>
-                                </div>
-                             
-                                <div className="col-md-6">
-                                    <div className="border p-4">
-                                        <div className="mt-4 mb-3">
-
-                                        {/* <span className="text-muted text-capitalize"> in {product.category}</span> */}
-                        
-
-                                            <h5 className="text-uppercase">
-                                                {product.title}
-                                            </h5>
-
-                                            {/* Product Price */}
-                                            <div className="price d-flex flex-row align-items-center">
-                                                <big className="display-6"><b>${product.price}</b></big>
-                                            </div>
-
-                                            {/* Product Description */}
-                                        <p className="text-muted">{product.description}</p>
-
-                                            {/* Ratings */}
-                                            Rating {product.rating && product.rating.rate}
-                                            <i className="fa fa-star text-warning"></i>  
-                                            <div className="cart mt-4 align-items-center"> <button className="btn btn-outline-dark text-uppercase mr-2 px-4">Add to Cart</button> </div>
-                                        </div>
-
+                
+                <div className="col-md-9 py-md-3">
+                    <div className="row">
+                        {filter.map((product) => {
+                            return (
+                                <div className="col-6 col-md-6 col-lg-4 mb-3" key={product.id}>
+                                    
+              <div className="card h-100">
                                         
-                                    </div>
-                                </div>
-                            </div>
+               <img src={product.image} className="m-3" style={{ height: "300px", width: "auto", objectFit: "contain" }} alt={product.title} />
+                 <div className="m-3 mb-0">
+                  <small className="card-title">{product.title.substring(0, 50)}...</small>
+                  </div>
+                     <div style={{ marginTop:"auto" }}>
+                    <div className="d-flex justify-content-between align-items-center">
+                       <NavLink className="stretched-link" to={`/product/${product.id}`}>
+                       
+                       <button className="btn btn-sm m-3 border-primary">
+                           <i className="fa fa-arrow-right text- left"></i>
+                         </button>
+                       </NavLink>
+                                                
+                        </div>
                         </div>
                     </div>
                 </div>
+                            )
+                        })}
+                    </div>
+
+                </div>
+
+
             </>
         )
-    }
+    }                       
 
     return (
-        <>
-            <div className="container px-0 mb-5" style={{ marginTop: "66px" }}>
-
-                {loading ? <Loading /> : <ShowDetails />}
-
+        <div className="container">
+            <div className="row">
+                {loading ? <Loading /> : <ShowProducts />}
             </div>
-        </>
+        </div>
     )
 }
 
-
-export default Product
+export default Products
